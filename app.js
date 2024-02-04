05a7110be275da93da4d3a353df4d6f752e8179c
prslv <presli75@gmail.com>
2024-02-04 02:52:35 +0200

    Made 'Enter' clickable, added 'Back to Home' button.
    Clicking on the 'Enter' text in the welcome page now triggers the enterApp() function. Added a back to home button in the error page.

--- app.js
+++ app.js
@@ -9,12 +9,22 @@ function enterApp() {
     const welcomeScreen = document.querySelector('.welcome-screen');
     const secondScreen = document.querySelector('.second-screen');
     const oopsScreen = document.querySelector('.oops');
+
     welcomeScreen.classList.add('hidden');
     oopsScreen.classList.add('hidden');
     secondScreen.classList.remove('hidden');
-
+    
     setTimeout(() => {
         secondScreen.classList.add('hidden');
         oopsScreen.classList.remove('hidden');
     }, 7500)
+}
+function restartApp() {
+    const welcomeScreen = document.querySelector('.welcome-screen');
+    const secondScreen = document.querySelector('.second-screen');
+    const oopsScreen = document.querySelector('.oops');
+    
+    welcomeScreen.classList.remove('hidden');
+    oopsScreen.classList.add('hidden');
+    secondScreen.classList.add('hidden');
 }
