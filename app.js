e4ef22820ddc9afca9255f1681ad057130f14460
prslv <presli75@gmail.com>
2024-02-04 02:28:16 +0200

    Welcome screen, styling and app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+document.addEventListener("keypress", function (event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+        event.preventDefault();
+        enterApp();
+    }
+});
+function enterApp() {
+    const welcomeScreen = document.querySelector('.welcome-screen');
+    const secondScreen = document.querySelector('.second-screen');
+    const oopsScreen = document.querySelector('.oops');
+    welcomeScreen.classList.add('hidden');
+    oopsScreen.classList.add('hidden');
+    secondScreen.classList.remove('hidden');
+
+    setTimeout(() => {
+        secondScreen.classList.add('hidden');
+        oopsScreen.classList.remove('hidden');
+    }, 7500)
+}
